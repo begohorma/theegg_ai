5abd29576be95fe5b8e8775e36b3c5f8a4dbb458
Begoña Hormaechea <begohorma@mac.com>
2020-10-03 15:14:59 +0200

    tarea_22: incluidos ficheros __init__.py para el correcto funcionamiento de las importaciones. Modificados los imports y llamadas

--- tarea_22/src/lechero.py
+++ tarea_22/src/lechero.py
@@ -1,13 +1,12 @@
-from src import auxFuntions as aux
-from src.maximizer import Maximizer
-
+from auxFuntions import get_non_negative_int, get_valid_list_of_ints
+from maximizer import Maximizer
 if __name__ == '__main__':
     # obtener datos de entrada
-    cows_total_num = aux.get_non_negative_int("Indique el número total de vacas a la venta: ")
-    lorry_total_weight = aux.get_non_negative_int("Indique el peso máximo que puede llevar el camión: ")
-    cows_weights = aux.get_valid_list_of_ints(
+    cows_total_num = get_non_negative_int("Indique el número total de vacas a la venta: ")
+    lorry_total_weight = get_non_negative_int("Indique el peso máximo que puede llevar el camión: ")
+    cows_weights = get_valid_list_of_ints(
         "Indique el peso en kg de las vacas separado por comas. Deben ser números enteros.: ", cows_total_num)
-    cows_production_day = aux.get_valid_list_of_ints(
+    cows_production_day = get_valid_list_of_ints(
         "Indique los litros de producción diaria de las vacas separado por comas. Deben ser números enteros.: ",
         cows_total_num)
 
